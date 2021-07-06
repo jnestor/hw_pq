7de953bccdd8de3a02f34f206d163fc928561dea
nestorj <nestorj@lafayette.edu>
2021-07-06 14:54:21 -0400

    minor indentation fix

--- sr_pq/sr_pq_stage.sv
+++ sr_pq/sr_pq_stage.sv
@@ -17,39 +17,39 @@ module sr_pq_stage (
 
     parameter STAGE = 0;  // should be overriden when instantiating
 
-  assign ki_lt_k = (kvi.key < kv.key);
+    assign ki_lt_k = (kvi.key < kv.key);
 
-  always_ff @(posedge clk)
+    always_ff @(posedge clk)
     begin
-      if (rst)
+        if (rst)
         begin
-          kv <= KV_EMPTY;
+            kv <= KV_EMPTY;
         end
-      else if (replace)
+        else if (replace)
         begin
             if (!ki_lt_k && !ki_lt_knext)
-                begin  // shift left
-                    kv <= kvnext;
-                end
+            begin  // shift left
+                kv <= kvnext;
+            end
             else if ((!ki_lt_k || (STAGE==1)) && ki_lt_knext) // always remove 1st stage
-                begin  // insert here for simultaneous enq, deq
-                    kv <= kvi;
-                end  // otherwise, this stage doesnt' change
+            begin  // insert here for simultaneous enq, deq
+                kv <= kvi;
+            end  // otherwise, this stage doesnt' change
         end
-      else if (deq)
+        else if (deq)
         begin
             kv <= kvnext;
         end
-      else if (enq)
+        else if (enq)
         begin
             if (!ki_lt_kprev && ki_lt_k)
-                begin
-                    kv <= kvi;
-                end
+            begin
+                kv <= kvi;
+            end
             else if (ki_lt_kprev && ki_lt_k)
-                begin
-                    kv <= kvprev;
-                end
+            begin
+                kv <= kvprev;
+            end
         end  // otherwise, this stage doesn't change
     end
 
