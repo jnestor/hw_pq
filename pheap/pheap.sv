d075b189becc1a991f1835a9ae0d8020b0d9173c
nestorj <nestorj@lafayette.edu>
2022-07-11 17:24:02 -0400

    minor annotations

--- pheap/pheap.sv
+++ pheap/pheap.sv
@@ -1,3 +1,13 @@
+//-----------------------------------------------------------------------------
+// Module Name   : pheap
+// Project       : pheap - pipelined heap priority queue implementation
+//-----------------------------------------------------------------------------
+// Author        : Ethan Miller
+// Created       : May 2021
+//-----------------------------------------------------------------------------
+// Description   : Top-level module for pipelined heap priority queue
+//-----------------------------------------------------------------------------
+
 `include "pheapTypes.sv"
 
 module pheap
@@ -17,19 +27,19 @@ module pheap
         end
     end
 
-    logic wenTop [LEVELS:1];  // write enable for level modules
-    logic start [LEVELS:1];   // assert to start operation at each levels
-    logic actives [LEVELS:1];  // inidicate whether each level is active
+    logic wenTop [LEVELS:1];     // write enables for memory (level modules)
+    logic start [LEVELS:1];      // starts operation at each level when 1
+    logic actives [LEVELS:1];    // indicate to memory (level module) if level is active
     logic shift_pos [LEVELS:1];  // initiate a shift at a level when asserted
     pheapTypes::done_t [LEVELS:1] done ;  // status for each level
-    pheapTypes::entry_t [LEVELS:1] a;     // not sure what this is used for ???
-    pheapTypes::entry_t [LEVELS:1] yTop ;  // current subtree root at each level???
-    pheapTypes::entry_t [LEVELS + 1:1] yBotL ;  // current left child at each level
-    pheapTypes::entry_t [LEVELS + 1:1] yBotR ;  // current right child at each lvel
+    pheapTypes::entry_t [LEVELS:1] a;     // write data for subtree root at each level
+    pheapTypes::entry_t [LEVELS:1] yTop ;  // read data fir current subtree root at each level
+    pheapTypes::entry_t [LEVELS + 1:1] yBotL ;  // read data for current left child at each level
+    pheapTypes::entry_t [LEVELS + 1:1] yBotR ;  // read data for current right child at each lvel
     logic [31:0] outs [LEVELS:1];  // level output for each level
-    assign priorityOut = outs[1];
-    assign yBotL[LEVELS + 1] = 'b0;  // bottom level childrent don't exist
-    assign yBotR[LEVELS + 1] = 'b0;
+    assign priorityOut = outs[1]; // top-level output
+    assign yBotL[LEVELS + 1] = 'b0;  // level below bottom level has capacity=0
+    assign yBotR[LEVELS + 1] = 'b0;  // level below bottom level has capacity=0
 
     genvar i;
     generate
